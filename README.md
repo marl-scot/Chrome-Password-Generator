8c8b4cb26721943af6a5ef5b882baac6e1fa74ef
Matt Lowe <marl.scot.1@googlemail.com>
2019-07-20 21:46:47 +0100

    Removed random password code for now Added NATO/Phonetic code Updated popup Installed new icons Added background page for easier debugging

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Chrome-Password-Generator
-A simple Chrome plugin that generates random passwords. It uses the dinopass API to generate pronounceable passwords or generates random character passwords. Available on the chrome web store.
+A simple Chrome plugin that generates random passwords. It uses the dinopass API to generate pronounceable passwords. Available on the chrome web store.
+This is cloned and adapted from : https://github.com/domkirby/Chrome-Password-Generator
+Domkirby's original extension gave the ability to grab a password from DinoPass or to generate a random password of a selected length, this version simply gives the DinoPass passwords
+Original extension available from : https://chrome.google.com/webstore/detail/password-generator-with-d/bchnjebifjbfmnnkiagdfjeohfjpfhfk
 
-Get it on the web store: https://chrome.google.com/webstore/detail/password-generator-with-d/bchnjebifjbfmnnkiagdfjeohfjpfhfk
+Get this version on the web store: 
+
+
+
+Pages that helped in developing this extension :
+https://stackoverflow.com/a/784547/6870483 - \n to <BR>
+http://jsfiddle.net/dandapianoman/U4gws/ - Oringinal NATA Alphabet scripts
+
+Icons : 
+https://www.iconfinder.com/icons/2639882/password_icon - WTFPL License
+
--- generate.js
+++ generate.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   var passwordButton = document.getElementById('newPass');
   var nonPronPassBtn = document.getElementById('newStrong');
+  var natoButton = document.getElementById('nato');
   $("#password").hide();
+  $("#conversionResults").hide();
   passwordButton.addEventListener('click', function() {
+    // Ensure phonetic results are hidden to avoid confusion with multiple button presses
+    $("#conversionResults").hide();
     $.ajax({
       url:"http://www.dinopass.com/password/strong",
       dataType: "text",
@@ -14,35 +18,15 @@ $(document).ready(function() {
         $("#password").show();
         copyToClipboard(data);
         $("#copied").html("Copied to Clipboard");
+        console.log(data);
       }
     })
   });
-  nonPronPassBtn.addEventListener('click', function() {
-    var length = document.getElementById('length').value;
-    if(length < 8) {
-      alert('Too Short!\nMinimum length is 8.');
-      return false;
-    }
-    if (length > 150) {
-      alert('Too Long!\nMax length is 150 to prevent crashing.');
-      return false;
-    }
-    var thepass = generateNonPronPassword(length);
-    $("#password").html(thepass);
-    $("#password").show();
-    copyToClipboard(thepass);
-    $("#copied").html("Copied to Clipboard");
-  });
+  natoButton.addEventListener('click', function() {
+    data=$("#password").html();
+    convertTextToNato(data);
 });
-
-function generateNonPronPassword(length) {
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&()",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-}
+});
 
 function copyToClipboard( text ){
                 var copyDiv = document.createElement('div');
@@ -55,3 +39,102 @@ function copyToClipboard( text ){
                 document.execCommand("Copy", false, null);
                 document.body.removeChild(copyDiv);
               }
+
+function convertTextToNato(textToConvert)
+{
+    data = textToNato(textToConvert);
+    document.getElementById('conversionResults').innerHTML = data.replace(/(\n)+/g, '<br />');
+    $("#conversionResults").show();
+}
+
+function textToNato(text)
+{
+    var newline = '<br />';
+    var results = '';
+    orig = text;
+    text = text.toLowerCase();
+    
+    for (var i=0; i < text.length; i++)
+    {
+        if (text.charAt(i) != orig.charAt(i)) {
+          results = results + "Upper Case ";
+        }
+        switch (text.charAt(i))
+        {
+            case 'a': results = results + 'Alfa\n'; break;
+            case 'b': results = results + 'Bravo\n'; break;
+            case 'c': results = results + 'Charlie\n'; break;
+            case 'd': results = results + 'Delta\n'; break;
+            case 'e': results = results + 'Echo\n'; break;
+            case 'f': results = results + 'Foxtrot\n'; break;
+            case 'g': results = results + 'Golf\n'; break;
+            case 'h': results = results + 'Hotel\n'; break;
+            case 'i': results = results + 'India\n'; break;
+            case 'j': results = results + 'Juliett\n'; break;
+            case 'k': results = results + 'Kilo\n'; break;
+            case 'l': results = results + 'Lima\n'; break;
+            case 'm': results = results + 'Mike\n'; break;
+            case 'n': results = results + 'November\n'; break;
+            case 'o': results = results + 'Oscar\n'; break;
+            case 'p': results = results + 'Papa\n'; break;
+            case 'q': results = results + 'Quebec\n'; break;
+            case 'r': results = results + 'Romeo\n'; break;
+            case 's': results = results + 'Sierra\n'; break;
+            case 't': results = results + 'Tango\n'; break;
+            case 'u': results = results + 'Uniform\n'; break;
+            case 'v': results = results + 'Victor\n'; break;
+            case 'w': results = results + 'Whiskey\n'; break;
+            case 'x': results = results + 'Xray\n'; break;
+            case 'y': results = results + 'Yankee\n'; break;
+            case 'z': results = results + 'Zulu\n'; break;
+            case ' ': results = results + 'Space\n'; break;
+            case '0': results = results + 'Zero\n'; break;
+            case '1': results = results + 'One\n'; break;
+            case '2': results = results + 'Two\n'; break;
+            case '3': results = results + 'Three\n'; break;
+            case '4': results = results + 'Four\n'; break;
+            case '5': results = results + 'Five\n'; break;
+            case '6': results = results + 'Six\n'; break;
+            case '7': results = results + 'Seven\n'; break;
+            case '8': results = results + 'Eight\n'; break;
+            case '9': results = results + 'Nine\n'; break;
+            case '!': results = results + 'Exclimation mark\n'; break;
+            case '"': results = results + 'Double Quotes\n'; break;
+            case '£': results = results + 'UK Pound\n'; break;
+            case '$': results = results + 'Dollar\n'; break;
+            case '%': results = results + 'Percent\n'; break;
+            case '^': results = results + 'Circumflex\n'; break;
+            case '&': results = results + 'And\n'; break;
+            case '*': results = results + 'Asterisk\n'; break;
+            case '(': results = results + 'Open Round Bracker\n'; break;
+            case ')': results = results + 'Close Round Bracket\n'; break;
+            case '-': results = results + 'Minus\n'; break;
+            case '_': results = results + 'Underscore\n'; break;
+            case '+': results = results + 'Plus\n'; break;
+            case '=': results = results + 'Equals\n'; break;
+            case '[': results = results + 'Open Square Bracket\n'; break;
+            case '{': results = results + 'Open Curly Bracket\n'; break;
+            case ']': results = results + 'Close Square Bracket\n'; break;
+            case '}': results = results + 'Close Curly Bracket\n'; break;
+            case ':': results = results + 'Colon\n'; break;
+            case ';': results = results + 'Semi-Colon\n'; break;
+            case '@': results = results + 'At Symbol\n'; break;
+            case '\'': results = results + 'Single Quote\n'; break;
+            case '<': results = results + 'Lower Than\n'; break;
+            case ',': results = results + 'Comma\n'; break;
+            case '>': results = results + 'Greater Than\n'; break;
+            case '.': results = results + 'Full Stop\n'; break;
+            case '/': results = results + 'Forward Slash\n'; break;
+            case '?': results = results + 'Question Mark\n'; break;
+            case '\\': results = results + 'Back Slash\n'; break;
+            case '|': results = results + 'Pipe\n'; break;
+            case '#': results = results + 'Hash\n'; break;
+            case '~': results = results + 'Tilda\n'; break;
+
+
+            default: results = results + 'NOT DEFINED' + '\n';
+        }
+    }
+    
+    return results;
+}
